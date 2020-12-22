9986bef2e14796c75f3df661c0f60084e33f8dc5
Oleksandra <karska.dev@gmail.com>
2020-12-22 00:16:58 -0800

    Solution with dictionary for TwoSum

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TwoSum
 {
@@ -6,7 +7,31 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int[] inputArr = { 2,7,11,15};
+            var result = TwoSum(inputArr, 9);
+            Console.WriteLine($"Indexes of two to make 9 are: {result[0]} and {result[1]}");
+        }
+        static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> inputNums = new Dictionary<int, int>();
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int secondArg = target - nums[i];
+                if (inputNums.ContainsKey(secondArg))
+                {
+                    result[0] = inputNums[secondArg];
+                    result[1] = i;
+                    return result;
+                }
+                else
+                {
+                    inputNums.Add(nums[i], i);
+                }
+            }
+            return null;
+
         }
     }
 }
