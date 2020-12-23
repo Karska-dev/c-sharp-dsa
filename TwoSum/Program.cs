e556619c386cd2a43594f85af5b006b960d3c7a8
Oleksandra <karska.dev@gmail.com>
2020-12-22 20:08:37 -0800

    Two solutions of TwoSum

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -8,26 +8,24 @@ namespace TwoSum
         static void Main(string[] args)
         {
             
-            int[] inputArr = { 2,7,11,15};
-            var result = TwoSum(inputArr, 9);
-            Console.WriteLine($"Indexes of two to make 9 are: {result[0]} and {result[1]}");
-            Console.WriteLine($"Indexes of two to make 9 are: {result[0]} and {result[1]}");
-            Console.WriteLine($"Indexes of two to make 9 are: {result[0]} and {result[1]}");
+            int[] inputArr = { 3,3,6,15};
+            int target = 9;
+            var result = TwoSum(inputArr, target);
+            //var result = BruteForce(inputArr, target);
+            Console.WriteLine($"Indexes of two to make {target} are: {result[0]} and {result[1]}");
         }
         static int[] TwoSum(int[] nums, int target)
         {
             Dictionary<int, int> inputNums = new Dictionary<int, int>();
-            int[] result = new int[2];
+            
             for (int i = 0; i < nums.Length; i++)
             {
                 int secondArg = target - nums[i];
                 if (inputNums.ContainsKey(secondArg))
                 {
-                    result[0] = inputNums[secondArg];
-                    result[1] = i;
-                    return result;
+                    return new int[] { inputNums[secondArg], i };
                 }
-                else
+                else if (!inputNums.ContainsKey(nums[i]))
                 {
                     inputNums.Add(nums[i], i);
                 }
@@ -35,5 +33,21 @@ namespace TwoSum
             return null;
 
         }
+        static int[] BruteForce(int[] nums, int target)
+        {
+            int[] result = new int[] { };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return result;
+        }
+      
     }
 }
